37b68f052661824e7f13e4c00d0d6f115f553030
John Purcell <john@caveofprogramming.com>
2022-12-22 11:40:19 +0100

    Variables

---
+++ 01-Introduction/11-Variables/variables.py
@@ -0,0 +1,7 @@
+#!./venv/bin/python
+
+greeting = "Hello World!"
+question = "How are you?"
+
+print(greeting)
+print(question)
---
+++ 01-Introduction/12-An-Interactive-Program/interactive.py
@@ -0,0 +1,10 @@
+#!./venv/bin/python
+
+greeting = "Hello "
+
+print("Enter your name: ")
+name = input()
+
+question = "How are you?"
+
+print(greeting + name + ". " + question)
